6e0b65141c2535f9f25bbe5ab320eab7be6246e2
iamcalegari <iamcalegari20@gmail.com>
2023-12-01 15:43:29 -0300

    wip

--- src/home.js
+++ src/home.js
@@ -1,50 +1,45 @@
 "use strict";
-const { CloudFunctionsServiceClient } = require("@google-cloud/functions").v1;
 
 const { GoogleAuth } = require("google-auth-library");
+const axios = require("axios");
 
 exports.home = async () => {
-  // [START cloudfunctions_v1_generated_CloudFunctionsService_CallFunction_async]
-  /**
-   * This snippet has been automatically generated and should be regarded as a code template only.
-   * It will require modifications to work.
-   * It may require correct/in-range values for request initialization.
-   * TODO(developer): Uncomment these variables before running the sample.
-   */
-  /**
-   *  Required. The name of the function to be called.
-   */
-  const auth = new GoogleAuth();
-  const name = "hello-world";
-  /**
-   *  Required. Input to be passed to the function.
-   */
-  // const data = "abc123";
-
-  // Imports the Functions library
-
-  // Instantiates a client
   try {
-    const functionsClient = new CloudFunctionsServiceClient();
-    const target = await functionsClient.getFunction({ name });
-    const client = await auth.getIdTokenClient(target.httpsTrigger);
-    const resp = await client.request({ url: target.httpsTrigger });
+    const accessToken = await getAccessToken();
+    const result = await callCloudFunction(accessToken);
 
-    // Construct request
-    // const request = {
-    //   name,
-    //   // data,
-    // };
-
-    // console.log(functionsClient.auth());
-
-    // // Run request
-    // const response = await functionsClient.callFunction(request);
-    console.log(resp.data);
-
-    return resp.data;
-    // [END cloudfunctions_v1_generated_CloudFunctionsService_CallFunction_async]
-  } catch (err) {
-    return err;
+    res.json({ result });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
+
+async function getAccessToken() {
+  const auth = new GoogleAuth();
+  const client = await auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+  });
+
+  const tokens = await client.getAccessToken();
+  return tokens.token;
+}
+
+async function callCloudFunction(accessToken) {
+  // Substitua este trecho com a lógica específica da sua Cloud Function
+  const cloudFunctionUrl =
+    "https://southamerica-east1-freela-arbitralis-406815.cloudfunctions.net/hello-world"; // Substitua pelo URL da sua Cloud Function
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const response = await axios.get(cloudFunctionUrl, { headers });
+
+  if (response.status !== 200) {
+    throw new Error(
+      `Cloud Function request failed with status ${response.status}`
+    );
+  }
+
+  return response.data;
+}
