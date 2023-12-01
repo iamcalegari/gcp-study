eea6df30dfeecbf9bf3500652dd9eed544a48fa5
iamcalegari <iamcalegari20@gmail.com>
2023-12-01 18:11:45 -0300

    wip-7

--- src/app.js
+++ src/app.js
@@ -10,7 +10,19 @@ app.get("/excel", async (req, res) => {
   const { body } = await home();
   const buffer = Buffer.from(body);
 
-  res.send(fs.writeFileSync("output.xlsx", buffer));
+  const filePath = "temp.xlsx";
+  fs.writeFileSync(filePath, buffer);
+
+  // Configurar os headers para a resposta
+  const fileDownloadName = "excel-file.xlsx";
+  res.download(filePath, fileDownloadName, (err) => {
+    // Remova o arquivo temporário após o download ser concluído ou se houver um erro
+    fs.unlinkSync(filePath);
+    if (err) {
+      console.error("Erro ao baixar o arquivo:", err);
+      res.status(500).send("Erro ao baixar o arquivo");
+    }
+  });
 });
 
 app.get("/", async (req, res) => {
